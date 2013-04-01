c1d8760170d95ee625c92604d139ae2d9452af56
Mike Lathrop <bigmike@bigmikestudios.com>
2013-04-01 15:45:25 -0700

    Fixed up Search.php

--- search.php
+++ search.php
@@ -3,18 +3,18 @@
 <div class="row">
   <div class="span8 content">
     <?php if ( have_posts() ) : ?>
-    <h1>Search Results for
-      <?php the_search_query(); ?>
-    </h1>
-    <?php /* Start the Loop */ ?>
-    <?php while ( have_posts() ) : the_post(); ?>
-    <h3><a href="<?php the_permalink(); ?>">
-      <?php the_title(); ?>
-      </a></h3>
-    <?php the_excerpt(); ?>
-    <hr/>
-    <?php endwhile; ?>
-    <?php
+        <h1>Search Results for
+          <?php the_search_query(); ?>
+        </h1>
+    
+		<?php /* Start the Loop */ ?>
+        <?php while ( have_posts() ) : the_post(); ?>
+        	<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+        	<?php the_excerpt(); ?>
+        	<hr/>
+    	<?php endwhile; ?>
+    	<?php
+			// Pagination:
 			global $wp_query;
 			$big = 999999999; // need an unlikely integer
 			echo paginate_links( array(
@@ -24,11 +24,13 @@
 				'total' => $wp_query->max_num_pages
 			) );
 			?>
-    <?php else : ?>
-    <h1>Nothing Found</h1>
-    <p>Sorry, but nothing matched your search criteria. Please try again with some different keywords</p>
-    <?php get_search_form(); ?>
-    <?php endif; ?>
+    	<?php else : ?>
+        
+			<?php /* No results. Boo. */ ?>
+    		<h1>Nothing Found</h1>
+    		<p>Sorry, but nothing matched your search criteria. Please try again with some different keywords</p>
+    		<?php get_search_form(); ?>
+   		 <?php endif; ?>
   </div>
   <div class="span4 sidebar">
     <?php
